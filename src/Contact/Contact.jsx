46d95c44adbd20aad3ed49b420c92a674b3b4612
guilbert970 <xavier-guilbert@orange.fr>
2021-11-24 20:54:11 +0100

    creation contact

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,12 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Contact.css";
 
-function Contact() {
-  return(
-    <>
-    <p>This is Contact</p>
-    <p>b</p>
-    </>
-  )
-}
+const Contact = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [newsletter, setNewletter] = useState("");
 
+  const handleFirstNameChange = (e) => setFirstName(e.target.value);
+  const handleLastNameChange = (e) => setLastName(e.target.value);
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handleMessageChange = (e) => setMessage(e.target.value);
+  const handleNewsletterChange = (e) => setNewletter(e.target.value);
+
+  return (
+    <form>
+      <h2>Formulaire</h2>
+      <div className="container">
+        <div className="colonnef">
+          <label htmlFor="firstName">
+            First Name
+            <input
+              type="texte"
+              id="firstName"
+              value={firstName}
+              onChange={handleFirstNameChange}
+            />
+          </label>
+        </div>
+        <div className="colonner">
+          <label htmlFor="lastName">
+            Last Name
+            <input
+              type="texte"
+              id="lastName"
+              value={lastName}
+              onChange={handleLastNameChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="email">
+            Email
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={handleEmailChange}
+              required
+            />
+          </label>
+        </div>
+
+        <label htmlFor="message">
+          Message
+          <textarea
+            type="message"
+            id="message"
+            value={message}
+            onChange={handleMessageChange}
+          />
+        </label>
+        <div className="checkbox">
+          <label htmlFor="newsletter">
+            <input
+              type="checkbox"
+              id="newsletter"
+              value={newsletter}
+              onChange={handleNewsletterChange}
+            />
+            S'inscricre a la newsletter
+          </label>
+        </div>
+      </div>
+      <button type="button">Submit</button>
+    </form>
+  );
+};
 export default Contact;
