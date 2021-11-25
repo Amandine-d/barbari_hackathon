2801c1eda8ede7acd62345e0413cf32606329607
guilbert970 <xavier-guilbert@orange.fr>
2021-11-25 21:24:58 +0100

    amelioration formulaire

--- src/components/Music/stylesInfos.jsx
+++ src/components/Music/stylesInfos.jsx
@@ -3,22 +3,28 @@ const styleInfos = [
     id: 1,
     style: "Classical",
     description:"Blabla", 
-    video:"https://www.youtube.com/embed/kjPTQLgGpq8",
+    video:"https://www.youtube.com/watch?v=Ci5yx5tJERQ",
     artist: 'wagner',
+    titre: 'La walkyrie',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/RichardWagner.jpg/260px-RichardWagner.jpg'
   },
   {
     id: 2,
     style: "Eighties",
     description:"Blabla", 
-    video:'"https://www.youtube.com/embed/kjPTQLgGpq8"',
+    video:'https://www.youtube.com/watch?v=dQsjAbZDx-4',
     artist: 'abba',
+    titre: 'Fernando',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/ABBA_-_TopPop_1974_5.png'
   },
   {
     id: 3,
     style: "Electro",
     description:"Blabla", 
-    video:'"https://www.youtube.com/embed/kjPTQLgGpq8"',
-    artist: ''
+    video:'',
+    artist: 'Kavinsky',
+    titre:"nightcall",
+    image:"https://upload.wikimedia.org/wikipedia/commons/0/02/Kavinsky.jpg"
   },
   {
     id: 4,
@@ -26,12 +32,16 @@ const styleInfos = [
     description:"Blabla", 
     video:'"https://www.youtube.com/embed/kjPTQLgGpq8"',
     artist: 'portishead',
+
   },
   {
     id: 5,
     style: "Techno",
     description:"Blabla", 
     video:'"https://www.youtube.com/embed/kjPTQLgGpq8"',
+    artist:"Enrico Sangiuliano",
+    titre:"Symbiosis",
+    image:"https://electro-news.eu/wp-content/uploads/2020/05/95897387_3322247554476649_7302532825222217728_o.jpg.webp"
   }
 ];
 
