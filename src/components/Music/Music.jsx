2058b2ced84aedeb20e300b9c15059ec55510357
[LL] Benjamin CORDREAUX <cordreaux.benjamin@gmail.com>
2021-11-25 16:21:36 +0100

    Réalisation Burger Menu

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 
 function Music() {
-
-  return(
+  return (
     <div>
-    <p>This is Music</p>
-    <li><a href='http://localhost:3000/music/techno'>Techno</a></li>
-    <li><a href='http://localhost:3000/music/triphop'>triphop</a></li>
-    <li><a href='http://localhost:3000/music/classical'>classical</a></li>
-    <li><a href='http://localhost:3000/music/eighties'>eighties</a></li>
-    <li><a href='http://localhost:3000/music/electro'>electro</a></li>
+      <p>This is Music</p>
+      <li>
+        <a href="http://localhost:3000/music/techno">Techno</a>
+      </li>
+      <li>
+        <a href="http://localhost:3000/music/triphop">Triphop</a>
+      </li>
+      <li>
+        <a href="http://localhost:3000/music/classical">Classical</a>
+      </li>
+      <li>
+        <a href="http://localhost:3000/music/eighties">Eighties</a>
+      </li>
+      <li>
+        <a href="http://localhost:3000/music/electro">Electro</a>
+      </li>
     </div>
-  )
+  );
 }
 
 export default Music;
