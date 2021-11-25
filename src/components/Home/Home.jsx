7dc08c2b306c0d9d86d4bd5cf2acf4043d5b134d
Sidney <sidney.croset@hotmail.com>
2021-11-25 21:19:25 +0100

    ajout de la page accueil

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import "./Home.css";
 
 function Home() {
-  return(
-    <p>This is Home</p>
-  )
+  return (
+    <div className="home">
+      <div className="column_1">
+        <div className="cercle">ecrire du texte</div>
+      </div>
+      <div className="line_2">
+        <h1 className="title_home">HOME</h1>{" "}
+      </div>
+      <div className="column_2">
+        <div className="cercle"> ecrire du texte</div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
